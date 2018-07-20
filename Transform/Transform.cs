0e485b7c83a33c56cd172bf6bf92c47d401323fd
rehman <rehmanmomin@gmail.com>
2018-07-20 15:29:48 +0530

    commit the solution

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,11 +4,22 @@ using System.Collections.Generic;
 
 namespace Transform
 {
+
+    public delegate int mapdel(int y); 
+
     public static class Transform 
     {
-        public static void Map()
+
+        public static List<int> Map(int[] input, mapdel operation)
         {
-            throw new NotImplementedException();
+            List<int> trans = new List<int>();
+            foreach (var item in input)
+            {
+                trans.Add(operation(n));
+            }
+
+            return trans;
+
         }
     }
 }
